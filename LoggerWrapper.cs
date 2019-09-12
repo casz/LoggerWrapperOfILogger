55f0e7aa2bee018c34b816868992422e12f5be5e
Joseph Petersen <josephp90@gmail.com>
2019-09-12 22:39:08 +0200

    implement LoggerWrapper

---
+++ LoggerWrapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace LoggerWrapperOfILogger
+{
+    public class LoggerWrapper<T> : ILogger<T>
+    {
+        private readonly ILogger _inner;
+        public LoggerWrapper(ILogger inner)
+        {
+            _inner = inner;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return _inner.BeginScope(state);
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return _inner.IsEnabled(logLevel);
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            _inner.Log(logLevel, eventId, state, exception, formatter);
+        }
+    }
+}
